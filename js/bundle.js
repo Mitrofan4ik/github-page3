42c0ac0f88ab29673daa3e2b8d479a88f22fb3ce
NIX\mytrofanova <ekaterinamytrofanova@nixsolutions.com>
2017-10-17 17:41:59 +0300

    add dite time

--- js/bundle.js
+++ js/bundle.js
@@ -17,18 +17,62 @@ for (var i = 0; i < linksBack.length; i++) {
 }
 
 // SORT LIST
-var dropDown = document.getElementsByClassName("sort-card__title")[0];
-var dropDownType = dropDown.getElementsByTagName("em")[0];
-var dropDownList =document.getElementsByClassName("sort-card-list")[0];
-var dropDownListItems =document.getElementsByClassName("sort-card-list__item");
-var li=0;
-for (li = 0; li < dropDownListItems.length; li++) {  
-  dropDownListItems[li].addEventListener("click", function() {       
-    dropDownType.innerHTML=this.getAttribute('data-sort');
-     dropDownList.classList.toggle('active');
+var myElem = document.getElementById('sortDropdown');
+if (myElem !== null) {
+
+  var dropDown = document.getElementsByClassName("sort-card__title")[0];
+  var dropDownType = dropDown.getElementsByTagName("em")[0];
+  var dropDownList =document.getElementsByClassName("sort-card-list")[0];
+  var dropDownListItems =document.getElementsByClassName("sort-card-list__item");
+  var li=0;
+
+  for (li = 0; li < dropDownListItems.length; li++) {  
+    dropDownListItems[li].addEventListener("click", function() {       
+      dropDownType.innerHTML=this.getAttribute('data-sort');
+        dropDownList.classList.toggle('active');
+    });
+  }
+
+
+  dropDown.addEventListener("click", function() {  
+    dropDownList.classList.toggle('active');
   });
 }
+// DATE TIME
+var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+"Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+];
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  var monthName=monthNames[today.getMonth()]
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML =
+  h + ":" + m;
+document.getElementById('date').innerHTML =
+  today.getDate() + " " + monthName;
+  var t = setTimeout(startTime, 20000);  
+}
+startTime()
 
-dropDown.addEventListener("click", function() {    
-  dropDownList.classList.toggle('active');
+// NAV LIST 
+var navListItem = document.getElementsByClassName("nav-hidden-list__item");
+navListItem.addEventListener("click", function() {
+  navListItem.classList.remove('active');
+  navListItem.classList.add('active');
 });
+// function setFocusToTextBox(){
+//   var textbox = document.getElementById("jsFeatured").focus();
+//   textbox.focus();
+//   textbox.scrollIntoView();
+// }
+// // function setFocusToTextBox(){
+// //   document.getElementById("jsFeatured").focus();
+// // }
