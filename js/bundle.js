cdd7e31514b1d22fc898a2bb46948477769347e7
NIX\mytrofanova <ekaterinamytrofanova@nixsolutions.com>
2017-10-17 13:25:48 +0300

    add script for sort list

--- js/bundle.js
+++ js/bundle.js
@@ -1,18 +1,34 @@
-/*FUNCTIONS*/
-
-/*END:FUNCTIONS*/
+// NAVIGATION
 var links = document.getElementsByClassName("-js-goto-innerpage");
 var i;
 for (i = 0; i < links.length; i++) {  
   links[i].addEventListener("click", function() {    
+    document.location.href = "https://mitrofan4ik.github.io/github-page/";
+  });
+}
+
+// NAVIGATION
+var linksBack = document.getElementsByClassName("-js-goto-startpage");
+
+for (var i = 0; i < linksBack.length; i++) {  
+  linksBack[i].addEventListener("click", function() {    
     document.location.href = "https://mitrofan4ik.github.io/github-page2/";
   });
 }
 
-var linksBack = document.getElementsByClassName("-js-goto-startpage");
-
-for (var i = 0; i < links.length; i++) {  
-  links[i].addEventListener("click", function() {    
-    document.location.href = "https://mitrofan4ik.github.io/github-page/";
+// SORT LIST
+var dropDown = document.getElementsByClassName("sort-card__title")[0];
+var dropDownType = dropDown.getElementsByTagName("em")[0];
+var dropDownList =document.getElementsByClassName("sort-card-list")[0];
+var dropDownListItems =document.getElementsByClassName("sort-card-list__item");
+var li=0;
+for (li = 0; li < dropDownListItems.length; li++) {  
+  dropDownListItems[li].addEventListener("click", function() {       
+    dropDownType.innerHTML=this.getAttribute('data-sort');
+     dropDownList.classList.toggle('active');
   });
 }
+
+dropDown.addEventListener("click", function() {    
+  dropDownList.classList.toggle('active');
+});
